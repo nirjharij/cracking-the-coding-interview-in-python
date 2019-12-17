9578bf8bc8378594cf699e770d792e95d078491a
Nirjhari Jankar <nirjhari.jankar@gmail.com>
2019-12-17 14:43:07 +0530

    adding linked list

--- linked_list.py
+++ linked_list.py
@@ -14,6 +14,7 @@ class LinkedList:
 			temp = temp.next
 
 	def push(self, new_data):
+		print("New data:{}".format(str(new_data)))
 		new_node = Node(new_data)
 		new_node.next = self.head
 		self.head = new_node
@@ -34,11 +35,48 @@ class LinkedList:
 
 		last.next = new_node
 
+	def delete(self, key):
+		print("deleting data: {}".format(str(key)))
+		temp = self.head
+		if temp:
+			if temp.data == key:
+				self.head = temp.next
+				temp = None
+				return
+
+		while temp:
+			if temp.data == key:
+				break
+			prev_node = temp
+			temp = temp.next
+
+		if temp == None:
+			return
+
+		prev_node.next = temp.next
+		temp = None
+
+	def reverse_print(self):
+		print("reverse_print")
+		if self.head == None:
+			return
+		temp = self.head
+		self.head = self.head.next
+		self.reverse_print()
+		print(temp.data)
+
+	def reverse(self):
+		while self.head:
+			temp = self.head
+			self.head = self.head.next
+			temp.next = None
+
 if __name__ == '__main__':
 	llist = LinkedList()
-	llist.head = Node(23)
-	second = Node(43)
-	third = Node(63)
-	llist.head.next = second
-	second.next = third
+	llist.push(3)
+	llist.push(5)
+	llist.push(7)
+	llist.push(1)
+	llist.delete(1)
 	llist.traverse()
+	llist.reverse_print()
