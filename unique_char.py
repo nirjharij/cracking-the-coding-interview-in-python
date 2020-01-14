e3d663d12f513b426f43ce83560b00f7024123bf
Nirjhari Jankar <Nirjhari.Jankar@in.ey.com>
2020-01-14 16:15:59 +0530

    adding unique string code

---
+++ unique_char.py
@@ -0,0 +1,38 @@
+def unique_char_with_ds(s):
+    char_flag = dict()
+
+    if len(s) > 128:
+        return False
+
+    for c in s:
+        if c in char_flag:
+            return False
+        else:
+            char_flag[c] = True
+    return True
+
+
+def unique_char_without_ds(s):
+    sorted_s = ''.join(sorted(s))
+    for i in range(len(sorted_s) - 1):
+        if sorted_s[i] == sorted_s[i+1]:
+            return False
+    return True
+
+
+# TC1 assuming only alphabets
+s1 = "ajabckajcaoichakcjakc"
+
+# TC2 assuming we also have special chars
+s2 = "alcacah9823yr2fhcck.cmalckjalcj"
+
+s3 = "abcdefghijklmnopqrstuvwxyz"
+
+s4 = "abcdefghijklmnopqrstuvwxyz ("
+
+result = unique_char_with_ds(s4)
+result2 = unique_char_without_ds(s4)
+print("result with ds: {}".format(str(result)))
+print("result without ds: {}".format(str(result2)))
+
+
